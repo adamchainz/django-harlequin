2978e2466aeaa9c276db4e715c92578cf34ecafc
Adam Johnson <me@adamj.eu>
2025-05-19 13:33:11 +0100

    Migrate formatting and linting to Ruff (#61)

--- src/django_harlequin/management/commands/harlequin.py
+++ src/django_harlequin/management/commands/harlequin.py
@@ -4,10 +4,8 @@ import os
 from argparse import ArgumentParser
 from typing import Any
 
-from django.core.management.base import BaseCommand
-from django.core.management.base import CommandError
-from django.db import DEFAULT_DB_ALIAS
-from django.db import connections
+from django.core.management.base import BaseCommand, CommandError
+from django.db import DEFAULT_DB_ALIAS, connections
 from django.db.backends.base.base import BaseDatabaseWrapper
 
 
